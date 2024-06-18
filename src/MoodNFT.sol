e29f369f076e8d81c7e015b714e912093ff284e9
viktorhugo <viktor1103@gmail.com>
2024-06-17 23:45:49 -0500

    add deploy contract

--- src/MoodNFT.sol
+++ src/MoodNFT.sol
@@ -22,20 +22,25 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.25;
 
-import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {ERC721, _isAuthorized} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import {Base64} from "@openzeppelin/contracts/utils/Base64.sol"; // para doficicar
 
 contract MoodNFT is ERC721 {
+
+    // Errors
+    error MoodNFT__CantFlipMoodIfNotOwner();
+    
+    uint256 private s_tokenCounter;
+    string private s_sadSvgImageUri;
+    string private s_happySvgImageUri;
+
     enum Mood {
         HAPPY,
         SAD
     }
 
-    uint256 private s_tokenCounter;
-    string private s_sadSvgImageUri;
-    string private s_happySvgImageUri;
+    // State variables
     mapping(uint256 => Mood) private s_tokenIdToMood;
-
     constructor(string memory sadSvgImageUri, string memory happySvgImageUri) ERC721("Mood NFT", "MN") {
         s_tokenCounter = 0;
         s_sadSvgImageUri = sadSvgImageUri;
@@ -46,6 +51,19 @@ contract MoodNFT is ERC721 {
         _safeMint(msg.sender, s_tokenCounter);
         s_tokenIdToMood[s_tokenCounter] = Mood.HAPPY;
         s_tokenCounter++;
+    }
+
+    function flipMood(uint256 tokenId) public {
+        // only the owner can change the mood
+        if (!_isAuthorized(msg.sender, tokenId)) {
+            revert MoodNFT__CantFlipMoodIfNotOwner();
+        }
+
+        if (s_tokenIdToMood[tokenId] == Mood.HAPPY) {
+            s_tokenIdToMood[tokenId] = Mood.SAD;
+        } else {
+            s_tokenIdToMood[tokenId] = Mood.HAPPY;
+        }
     }
 
     function _baseURI() internal pure override returns (string memory) {
